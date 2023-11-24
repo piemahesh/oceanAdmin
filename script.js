97c051f7c21a44edc8c172a66ececafd1fbf5fac
manopadma <ag102.manogari@gmail.com>
2023-11-24 15:37:03 +0530

    main changes

--- script.js
+++ script.js
@@ -39,56 +39,80 @@ const companyContainer = document.querySelector("#companyContainer");
 const listView = document.querySelector("#listView");
 const gridView = document.querySelector("#gridView");
 const cardContainer = document.querySelector("#items");
-// list display
-data.forEach((item) => {
-    const lhtml = `
-    <div class ="flex font-light py-5 pl-10 border border-transparent border-b-[#3f4e8e60] hover:border-b-[#3f4d8e] hover:cursor-pointer hover:shadow-md hover:transition-all
+const interestedIn = document.querySelector("#interest");
+console.log(interestedIn);
+console.log(interestedIn.innerHTML)
+
+
+function listComponent(item, flag) {
+    return `<div class ="flex font-light items-center py-5 pl-10 border border-transparent border-b-[#3f4e8e60] hover:border-b-[#3f4d8e] hover:cursor-pointer hover:shadow-md hover:transition-all
     hover:duration-300 mb-2 " "id="datasHandle">
-   
         <div class="flex md:w-1/4"><span class="hover:text-[#397c8f] transition-colors duration-300">${item.fullName}</span></div>
         <div class="flex md:w-1/4"><span class="hover:text-[#397c8f] transition-colors duration-300">${item.companyName}</span></div>
         <div class="flex md:w-1/4"><span class="hover:text-[#397c8f] transition-colors duration-300">${item.companyEmail}</span></div>
-        <div class="flex md:w-1/4"><span class="hover:text-[#397c8f] transition-colors duration-300">${item.interestedIn}</span></div>
-        
-    </div>`;
-    listContainer.innerHTML = listContainer.innerHTML + lhtml;
-});
-// grid display
-data.forEach((item)=>{
-    const ghtml = `
-    <div class ="h-60 max-w-[300px] min-w-full flex flex-col pt-3  font-light text-slate-950 rounded-lg justify-between overflow-hidden
-    transition-all duration-300 hover:scale-[1.01] hover:shadow-lg border" "id="datasHandle">
-   
-        <div class="flex flex-col ml-5 order-2 ">
-        <span class = "text-[#3f4d8e] font-semibold">Full Name</span>
-        <span> ${item.fullName}grid</span>
+        <div class="flex md:w-1/4 ${flag ? `hidden` : `flex`}">
+        <span class="hover:text-[#397c8f] transition-colors duration-300">${item.interestedIn}</span>
         </div>
-
-        <div class=" flex  order-1  justify-between border border-transparent border-b-indigo-800">
-        <div>
-        <span class= "ml-5 text-[#3f4d8e] font-semibold">${item.companyName}</span>
-        <span class= "flex gap-1 ml-5 ">
-        <i class="ri-time-line text-slate-500"></i>
-        <p>2 hours ago</p>
-        </span>
+         <div class="w-1/4 flex ${flag ? `flex` : `hidden`}">
+            <button class=" ring rounded-md w-1/4">View</button>
+            <button class=" ml-7 py-1 bg-green-700 text-white rounded-md w-1/4">Accept</button>
         </div>
-        <i class="ri-notification-2-line mr-3 font-semibold text-[#397c8f]"></i>
-        </div>
+    </div>`
+}
+function gridComponent(item, flag) {
+    return `<div class ="h-60 max-w-[300px] min-w-full flex flex-col pt-3  font-light text-slate-950 rounded-lg justify-between overflow-hidden
+        transition-all duration-300 hover:scale-[1.01] hover:shadow-lg border" "id="datasHandle">
+       
+            <div class="flex flex-col ml-5 order-2 ">
+            <span class = "text-[#3f4d8e] font-semibold">Full Name</span>
+            <span> ${item.fullName}grid</span>
+            </div>
+    
+            <div class=" flex  order-1  justify-between border border-transparent border-b-indigo-800">
+            <div>
+            <span class= "ml-5 text-[#3f4d8e] font-semibold">${item.companyName}</span>
+            <span class= "flex gap-1 ml-5 ">
+            <i class="ri-time-line text-slate-500"></i>
+            <p>2 hours ago</p>
+            </span>
+            </div>
+            <i class="ri-notification-2-line mr-3 font-semibold text-[#397c8f]"></i>
+            </div>
+    
+            
+    
+            <div class="flex flex-col ml-5 order-4 ">
+            <span class = "text-[#3f4d8e] font-semibold">Their Intrested In</span>
+            <span>${item.interestedIn}</span>
+            </div>
+            <div class = "order-5 flex">
+            <button class="flex  items-center justify-center bg-[#397c8f] p-2 w-full font-semibold text-lg text-white" id= "viewBtn">View</button>
+            <button class="flex  items-center justify-center bg-[#3f4d8e]  w-full font-semibold text-lg text-white ${flag ? `flex` : `hidden`}">Accept</button> 
+            </div>
+            
+        </div>`;
+}
+newRequest.addEventListener("click", (e) => {
+    listContainer.innerHTML = "";
+    girdContainer.innerHTML = "";
+    interestedIn.innerHTML = "View or Edit"
+    data.slice(5).map((item) => {
+        listContainer.innerHTML = listContainer.innerHTML + listComponent(item, true);
+        girdContainer.innerHTML = girdContainer.innerHTML + gridComponent(item, true);
+    });
 
-        
-
-        <div class="flex flex-col ml-5 order-4 ">
-        <span class = "text-[#3f4d8e] font-semibold">Their Intrested In</span>
-        <span>${item.interestedIn}</span>
-        </div>
-
-        <button class="flex  order-5 md:order-5 items-center justify-center bg-[#397c8f] p-2 w-full font-semibold text-lg text-white" id= "viewBtn">View</button>
-        
-    </div>`;
-    girdContainer.innerHTML = girdContainer.innerHTML + ghtml;
+    e.preventDefault()
 })
+
+addEventListener("DOMContentLoaded", () => {
+    data.slice(5).map((item) => {
+        listContainer.innerHTML = listContainer.innerHTML + listComponent(item, false);
+        girdContainer.innerHTML = girdContainer.innerHTML + gridComponent(item, false);
+    })
+});
+
 // gridView display
-gridView.addEventListener("click",()=>{
+gridView.addEventListener("click", () => {
     gridView.classList.remove("bg-slate-300")
     gridView.classList.add("bg-[#3f4d8e]", "text-white", "transition-all", "duration-700");
     listView.classList.remove("bg-[#3f4d8e]", "text-white");
@@ -98,7 +122,7 @@ gridView.addEventListener("click",()=>{
     girdContainer.classList.remove("md:hidden");
 })
 // listView display
-listView.addEventListener("click",()=>{
+listView.addEventListener("click", () => {
     listView.classList.remove("bg-slate-300")
     listView.classList.add("bg-[#3f4d8e]", "text-white", "transition-all", "duration-700");
     gridView.classList.remove("bg-[#3f4d8e]", "text-white");
